637ca5d5565388052cdb8b6d14cdd77055943d9b
miso-taku <47471805+miso-taku@users.noreply.github.com>
2022-04-25 19:52:14 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import streamlit as st
 import matplotlib.pyplot as plt
 from PIL import Image
 from model import predict
+import plotly.graph_objects as go
+
 
 st.set_option("deprecation.showfileUploaderEncoding", False)
 
@@ -14,7 +16,7 @@ st.sidebar.write("")
 img_file = st.sidebar.file_uploader("画像を選択", type=["png", "jpg"])
 
 if img_file is not None:
-    with st.spinner("推定中"):
+    with st.spinner("判定中"):
         img = Image.open(img_file)
         st.image(img, caption="対象の画像", width=480)
         st.write("")
@@ -22,7 +24,7 @@ if img_file is not None:
         results = predict(img)
 
         st.subheader("判定結果")
-        n_top = 3
+        n_top = 5
         for result in results[:n_top]:
             st.write(str(round(result[1]*100, 2)) + "%の確率で" + result[0] + "です。")
 
@@ -30,10 +32,6 @@ if img_file is not None:
         pie_labels.append("others")
         pie_probs =  [result[1] for result in results[:n_top]]
         pie_probs.append(sum([result[1] for result in results[n_top:]]))
-        fig, ax = plt.subplots()
-        wedgeprops = {"width":0.3, "edgecolor":"white"}
-        textprops = {"fontsize":6}
-
-        ax.pie(pie_probs, labels=pie_labels, counterclock=False, startangle=90,
-               textprops=textprops, autopct="%.2f", wedgeprops=wedgeprops)  # 円グラフ
-        st.pyplot(fig)
+        fig = go.Figure(data=[go.Pie(labels=pie_labels, values=pie_probs,
+                                    direction='clockwise', hole=0.5)])
+        st.plotly_chart(fig)
